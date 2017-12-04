603640dc457aea68c36225a0b03457820e6cce1b
zouhunter <106362705@qq.com>
2017-12-04 21:17:26 +0800

    更新demo

--- UnityProject/Assets/Demo/Scripts/Demo.cs
+++ UnityProject/Assets/Demo/Scripts/Demo.cs
@@ -12,6 +12,115 @@ using System.Collections;
 using System.Collections.Generic;
 using BridgeUI.Model;
 using BridgeUI;
+using System;
+
+//public class DataDic : IDictionary
+//{
+//    public object this[object key]
+//    {
+//        get
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        set
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public int Count
+//    {
+//        get
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public bool IsFixedSize
+//    {
+//        get
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public bool IsReadOnly
+//    {
+//        get
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public bool IsSynchronized
+//    {
+//        get
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public ICollection Keys
+//    {
+//        get
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public object SyncRoot
+//    {
+//        get
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public ICollection Values
+//    {
+//        get
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+
+//    public void Add(object key, object value)
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Clear()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public bool Contains(object key)
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void CopyTo(Array array, int index)
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public IDictionaryEnumerator GetEnumerator()
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    public void Remove(object key)
+//    {
+//        throw new NotImplementedException();
+//    }
+
+//    IEnumerator IEnumerable.GetEnumerator()
+//    {
+//        throw new NotImplementedException();
+//    }
+//}
+
 public class Demo : MonoBehaviour
 {
     private const string pane01 = "Panel01";

a5270f824cee99d74e8937a4ff3fa05e19f0afe7
zouhunter <1063627025@qq.com>
2018-01-09 20:05:13 +0800

    update

--- UnityProject/Assets/Core/Graph/Editor/Drawers/LunchDrawer.cs
+++ UnityProject/Assets/Core/Graph/Editor/Drawers/LunchDrawer.cs
@@ -1,20 +1,20 @@
 ﻿
-using NodeGraph;
-using NodeGraph.DataModel;
-using UnityEditor;
+//using NodeGraph;
+//using NodeGraph.DataModel;
+//using UnityEditor;
 
-[CustomNodeGraphDrawer(typeof(Luncher))]
-public class LuncherDawer : NodeDrawer
-{
-    public override string Category
-    {
-        get
-        {
-            return "empty";
-        }
-    }
-    public override void OnInspectorGUI(NodeGUI gui)
-    {
-        EditorGUILayout.HelpBox("Any Lunch: Lunch SubPanels From Any State", MessageType.Info);
-    }
-}
+//[CustomNodeGraphDrawer(typeof(Luncher))]
+//public class LuncherDawer : NodeDrawer
+//{
+//    public override string Category
+//    {
+//        get
+//        {
+//            return "empty";
+//        }
+//    }
+//    public override void OnInspectorGUI(NodeGUI gui)
+//    {
+//        EditorGUILayout.HelpBox("Any Lunch: Lunch SubPanels From Any State", MessageType.Info);
+//    }
+//}

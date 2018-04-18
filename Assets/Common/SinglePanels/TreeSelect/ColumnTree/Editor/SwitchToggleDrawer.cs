ae4304f9dee77099fba1832e2287d140d1a892bb
zouhangte <zouhantezbm@126.com>
2018-04-18 12:59:46 +0800

    添加一个可切换文字颜色的脚本

---
+++ Assets/Common/SinglePanels/TreeSelect/ColumnTree/Editor/SwitchToggleDrawer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+namespace BridgeUI.Common.Tree
+{
+
+    #region UNITY_EDITOR
+    [UnityEditor.CustomEditor(typeof(SwitchToggle))]
+    public class SwitchToggleDrawer : UnityEditor.Editor
+    {
+        private UnityEditor.SerializedProperty labelProp;
+        private UnityEditor.SerializedProperty off_Lable_Color;
+        private UnityEditor.SerializedProperty on_Lable_Color;
+        private Editor defultDrawer;
+        void OnEnable()
+        {
+            labelProp = serializedObject.FindProperty("label");
+            on_Lable_Color = serializedObject.FindProperty("on_Lable_Color");
+            off_Lable_Color = serializedObject.FindProperty("off_Lable_Color");
+        }
+        public override void OnInspectorGUI()
+        {
+            UnityEditor.EditorGUILayout.PropertyField(labelProp);
+            if(labelProp.objectReferenceValue != null)
+            {
+                UnityEditor.EditorGUILayout.PropertyField(on_Lable_Color);
+                UnityEditor.EditorGUILayout.PropertyField(off_Lable_Color);
+
+            }
+        }
+    }
+    #endregion
+
+}
